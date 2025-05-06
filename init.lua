6093ef10c2c4f41c4796fc56b72e3836ef7897b6
Hooninator <jbellavita@berkeley.edu>
2025-05-06 05:50:59 -0700

    update

--- init.lua
+++ init.lua
@@ -11,12 +11,21 @@ vim.opt.splitright = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.scrolloff = 8
+vim.opt.rnu = true
+vim.opt.nu = true
 vim.opt.clipboard = "unnamedplus"
 
 
+vim.cmd([[highlight Statement ctermfg=Red]])
+vim.cmd([[colorscheme retrobox]])
+vim.cmd([[set background=dark]])
+vim.cmd([[inoremap jk <esc>]])
 
 
-vim.cmd([[colorscheme gruvbox]])
+
+vim.cmd([[colorscheme retrobox]])
+--vim.cmd([[colorscheme default]])
+--vim.cmd([[colorscheme industry]])
 
 
 vim.cmd [[
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,5 +5,14 @@ return {
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
   { "lewis6991/gitsigns.nvim" },
   { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-  { "neoclide/coc.nvim", build="npm ci", lazy=false}
+  { "neoclide/coc.nvim", build="npm ci", lazy=false},
+  {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+  }
 }
